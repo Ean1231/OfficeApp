57f1e20de8b17bf00da1affaeb1706179c733b30
Ean1231 <macdonaldbosman@gmail.com>
2022-10-18 09:49:56 +0200

    EanBos

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -3,11 +3,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Firestore, collection, collectionData, doc, docData, addDoc, deleteDoc, updateDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
-    export interface Employees {
-      id?: string;
-      firstname: string;
-      lastname: string;
-    }
+ 
 
 @Injectable({
   providedIn: 'root'
@@ -17,35 +13,10 @@ collection = 'Offices';
 collectionData = 'employees'
   constructor(private firestore: AngularFirestore, private fire: Firestore) { }
 
-  getEmployees(): Observable<Employees[]> {
-    const notesRef = collection(this.fire, 'employees');
-    return collectionData(notesRef, { idField: 'id'}) as Observable<Employees[]>;
-  }
-
-  getEmployeeById(id): Observable<Employees> {
-    const employeeDocRef = doc(this.fire, `employees/${id}`);
-    return docData(employeeDocRef, { idField: 'id' }) as Observable<Employees>;
-  }
-
-  // addEmployee(employees: Employees) {
-  //   const employeessRef = collection(this.fire, 'employees');
-  //   return addDoc(employeessRef, employees);
-  // }
-
-  deleteEmployess(employees: Employees) {
-    const employeesDocRef = doc(this.fire, `employees/${employees.id}`);
-    return deleteDoc(employeesDocRef);
-  }
-
-  updateEmployee(employees: Employees) {
-    const employeesDocRef = doc(this.fire, `employees/${employees.id}`);
-    return updateDoc(employeesDocRef, { firstname: employees.firstname, lastname: employees.lastname });
-  }
-
+  //Employees service
   update_employee(id,data){
     return this.firestore.doc(this.collectionData + '/' + id).update(data);
   }
-
 
   getemployees(){
     return this.firestore.collection(this.collectionData).snapshotChanges();
@@ -59,7 +30,11 @@ collectionData = 'employees'
     return this.firestore.doc(this.collectionData + '/' + id).delete();
   }
 
+  get_single_employee(id){
+    return this.firestore.collection(this.collectionData).doc(id).valueChanges();
+  }
 
+  //Office Service
   getOffices(){
     return this.firestore.collection(this.collection).snapshotChanges();
   }
@@ -75,10 +50,6 @@ collectionData = 'employees'
 
   get_single_office(id){
     return this.firestore.collection(this.collection).doc(id).valueChanges();
-  }
-
-  get_single_employee(id){
-    return this.firestore.collection(this.collectionData).doc(id).valueChanges();
   }
 
   update_office(id,data){
